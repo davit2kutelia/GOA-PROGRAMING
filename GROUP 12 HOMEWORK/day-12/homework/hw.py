eb265ad1b0424c4e3f251200cf29c3b57bd6b4ad
davit2kutelia <datakutelia8@gmail.com>
2024-11-04 00:34:00 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-12/homework/hw.py
@@ -0,0 +1,21 @@
+
+# correct_password = "mypassword"
+
+# user_input = input("Please enter the password: ")
+
+# while user_input != correct_password:
+#     print("Incorrect password, please try again.")
+#     user_input = input("Please enter the password: ")
+
+# print("Access granted!")
+
+
+
+# user_input = int(input("please enter number : "))
+# for i in  range(user_input + 1):
+#     print(i)
+
+
+num = int(input('please enter number :'))
+total_sum= sum(range(num))
+print(total_sum)
