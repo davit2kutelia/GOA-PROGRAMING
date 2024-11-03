aa683932c32b4d12c1f95c7ac326ae535ff6f7f1
davit2kutelia <datakutelia8@gmail.com>
2024-11-03 23:55:33 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-5/homework/hw.py
@@ -0,0 +1,5 @@
+mom = 30 
+dad = 35
+sister= 17
+
+print(mom + 5, dad + 5, sister + 5)
---
+++ GROUP 12 HOMEWORK/day-8/homework/hw.py
@@ -0,0 +1,22 @@
+# current_age = int(input("What is your current age? "))
+# years_to_travel_back = int(input("How many years do you want to travel back in time? "))
+# age_after_travel = current_age - years_to_travel_back
+# print("You will be", age_after_travel, "years old after time travel.")
+
+
+# integer_var = 42
+# float_var = 3.14
+# string_var = "Hello, World!"
+# list_var = [1, 2, 3, 4, 5]
+# tuple_var = (1, 2, 3)
+# dict_var = {"name": "Alice", "age": 30}
+# bool_var = True
+
+# print("Data type of integer_var:", type(integer_var))
+# print("Data type of float_var:", type(float_var))
+# print("Data type of string_var:", type(string_var))
+# print("Data type of list_var:", type(list_var))
+# print("Data type of tuple_var:", type(tuple_var))
+# print("Data type of dict_var:", type(dict_var))
+# print("Data type of bool_var:", type(bool_var))
+
