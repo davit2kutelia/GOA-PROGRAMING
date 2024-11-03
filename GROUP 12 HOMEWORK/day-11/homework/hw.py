266bb0c0c8e9012d9009ed96cf045d5df8465665
davit2kutelia <datakutelia8@gmail.com>
2024-11-04 00:13:12 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-11/homework/hw.py
@@ -0,0 +1,13 @@
+# academy = input("which is best academy?:")
+# while academy == "":
+#     print('answer is goa')
+#     academy = input("which is best academy?:")
+#     print('u guessed right')
+
+# word = "GOA"
+# reversed_word = ""
+
+# for char in word:
+#     reversed_word = char + reversed_word
+
+# print(reversed_word)
