e55fc13f7e02654014871ce34e0541a2b4f3579d
davit2kutelia <datakutelia8@gmail.com>
2024-11-07 23:09:09 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-17/homework/hw.py
@@ -0,0 +1,11 @@
+my_list = [42, "Hello", 3.14, True, None]
+
+print("მომენტური სია:", my_list)
+
+my_list[0] = 100
+my_list[1] = "Goodbye"
+my_list[2] = 6.28
+my_list[3] = False
+my_list[4] = "Changed"
+
+print("შეცვლილი სია:", my_list)
