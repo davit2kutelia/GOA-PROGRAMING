2029e5a757db66be5a3e6f8968b153c28f2336c7
davit2kutelia <datakutelia8@gmail.com>
2024-11-13 20:45:36 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-18/homework/hw.py
@@ -0,0 +1,7 @@
+fav_animasls=["dog", "cat", "monkey", "elephant", "dophin"]
+favorite_foods = ["Pizza", "Chocolate", "Cottage Cheese", "Khinkali", "Pasta"]
+
+print(favorite_foods[0:1])
+input("want to know recipie? :")
+if input == yes:
+    print(cheese, comi, akksni,aijaia,oakoak,oakoak)
