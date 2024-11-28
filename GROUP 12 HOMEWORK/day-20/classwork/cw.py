8408b8fd324515e167a4664601a050798357b7f9
davit2kutelia <datakutelia8@gmail.com>
2024-11-28 23:59:31 +0400

    second commit

---
+++ GROUP 12 HOMEWORK/day-20/classwork/cw.py
@@ -0,0 +1,40 @@
+my_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# 1
+# for i in my_list:
+
+        #     if i % 2 == 0:
+#         print(i)
+
+# 2
+# if i % 5 == 0:
+#     print
+
+# 3
+# summ = []  
+# summ.append(sum(my_list))
+# print(summ)
+
+# 4
+# even_list=[]
+#     if i % 2 ==0:
+#         even_list.append(i)
+        
+# print(sum(even_list))
+
+
+# 5
+# even_numbers=[]
+# for i in my_list:
+#     if i % 2 == 1:
+#         even_numbers.append(i)
+    
+# print(even_numbers)
+
+# 6
+# divided = []
+# for i in my_list:
+#     if i % 3 == 0 or i % 5 == 0:
+#         divided.append(i)
+# print(divided)
+
+
