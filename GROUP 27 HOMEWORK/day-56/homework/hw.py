b8f09cf0cbfd2cdfa989619d349b47bf7e01ab03
davit2kutelia <datakutelia8@gmail.com>
2025-01-10 14:48:25 +0400

    second commit

---
+++ GROUP 27 HOMEWORK/day-56/homework/hw.py
@@ -0,0 +1,22 @@
+# fruits = ["apple", "banan" ,"orange" ,"watermelon", "anans,"]
+# print(fruits[2], fruits[4])
+
+# number = int(input("please tell number  :"))
+# for i in range(0, number + 1):
+#     if i % 2 == 1:
+#         print(i)
+
+
+# numbers = (1,2,3,4,5,6)
+# for i in numbers:
+#     print(i * 2)
+
+
+# n = [5,7,3,4,9,7,1]
+# print(n[::-1])
+
+
+n = [1,2,3,4,5,6]
+n[0], n[-1] = n[-1], n[0]
+print(n)
+
