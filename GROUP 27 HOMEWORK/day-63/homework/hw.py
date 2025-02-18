c31dd8534d9cbbde24aec135d2779d764ba1b3f2
davit2kutelia <datakutelia8@gmail.com>
2025-02-18 13:03:47 +0400

    second commit

--- GROUP 27 HOMEWORK/day-63/homework/hw.py
+++ GROUP 27 HOMEWORK/day-63/homework/hw.py
@@ -12,18 +12,18 @@
 
 
 
-def winner(player1, player2):
-    player1_score = player1[0] + player1[1] + (player1[0] + player1[1]) * (player1[2] / 10)
-    player2_score = player2[0] + player2[1] + (player2[0] + player2[1]) * (player2[2] / 10)
+# def winner(player1, player2):
+#     player1_score = player1[0] + player1[1] + (player1[0] + player1[1]) * (player1[2] / 10)
+#     player2_score = player2[0] + player2[1] + (player2[0] + player2[1]) * (player2[2] / 10)
     
-    if player1_score > player2_score:
-        return "Player 1 wins"
-    elif player2_score > player1_score:
-        return "Player 2 wins"
-    else:
-        return "Tie"
+#     if player1_score > player2_score:
+#         return "Player 1 wins"
+#     elif player2_score > player1_score:
+#         return "Player 2 wins"
+#     else:
+#         return "Tie"
 
-player1 = [5, 6, 8]  
-player2 = [10, 7, 6]  
+# player1 = [5, 6, 8]  
+# player2 = [10, 7, 6]  
 
-print(winner(player1, player2))
+# print(winner(player1, player2))
