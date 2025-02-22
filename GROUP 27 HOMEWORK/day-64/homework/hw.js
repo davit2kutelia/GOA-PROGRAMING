fc8986887707de69e07bd2890c7b1346f0dcea46
davit2kutelia <datakutelia8@gmail.com>
2025-02-22 11:44:41 +0400

    second commit

---
+++ GROUP 27 HOMEWORK/day-64/homework/hw.js
@@ -0,0 +1,50 @@
+// let wether = "rain"
+// if(wether == "sunny"){
+//     console.log("today wether will be sunny")
+// }
+
+// else{
+//     console.log("today will be rainy day")
+// }
+
+
+
+// let text = document.getElementById("h1");
+
+// if (text.textContent == "") {
+//     text.textContent = "I'm gonna fill you with text!";
+// }
+
+
+// let item = "chaquchi"
+// let allowed = "chaquchi"
+// let seconditem = "lursmani"
+// let disallowed = "lursmani"
+// function func(allowed,disallowed){
+//     if(item = allowed){
+//         console.log("this item is allowed")
+
+//     }
+//     else if(seconditem = disallowed){
+//         console.log("this item is disslowed")
+
+//     }
+// }
+
+// func(item, allowed, seconditem, disallowed);
+
+
+
+// let sleeptime = 11
+// function func(){
+//     if(sleeptime >= 11){
+//     console.log("what u doing its sleep time")
+//     }
+
+//     else if(sleeptime < 11){
+//         console.log("u can be awake")
+//     }
+// }   
+ 
+
+// func(sleeptime)
